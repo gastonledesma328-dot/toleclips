52ba20586e8279286d39b72528f53189dc3d5f76
gastonledesma328-dot <gastonledesma328@gmail.com>
2026-02-11 23:29:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -142,4 +142,6 @@ def serve_video(video_id):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
